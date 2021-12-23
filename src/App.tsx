b2b7967a8175e86e853fcaeaf0757a58f1484028
szymonhel <szhelmecki@gmail.com>
2021-12-23 15:18:24 +0100

    Configured tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,28 @@
 import React from 'react';
 import './App.css';
-import {SearchBar} from './componets/SearchBar';
+import { SearchBar } from './componets/SearchBar';
 
 var QRCode = require('qrcode.react');
 
-
 type Props = {};
 type State = {
-    qrCodeValue: string;
+  qrCodeValue: string;
 };
 
 export class App extends React.Component<Props, State> {
-    constructor(props: any) {
-        super(props);
-        this.state = {qrCodeValue: ''};
-    }
+  constructor(props: any) {
+    super(props);
+    this.state = { qrCodeValue: '' };
+  }
 
-    render() {
-        return (
-            <>
-                <SearchBar onSubmit={(val) => this.setState({qrCodeValue: val})}/>
-                <QRCode value={this.state.qrCodeValue}/>
-            </>
-        );
-    };
-};
+  render() {
+    return (
+      <>
+        <SearchBar onSubmit={(val) => this.setState({ qrCodeValue: val })} />
+        <QRCode value={this.state.qrCodeValue} />
+      </>
+    );
+  }
+}
 
 export default App;
